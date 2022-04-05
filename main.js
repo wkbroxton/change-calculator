4ae2ad56928eb4e212b2c9d9e57b9044ed91f4b2
William Kevin Broxton <90276407+wkbroxton@users.noreply.github.com>
2022-04-05 13:32:57 -0700

    first new commit

---
+++ main.js
@@ -0,0 +1,63 @@
+// Write your JavaScript here
+
+const chaChing = new Audio(
+  "https://docs.google.com/uc?export=download&id=1L6d0xtsOvnKTMnLSa1KidM09jcbXgQp-"
+);
+
+chaChing.volume = 0.1;
+
+const player = new Audio();
+
+const calculateChange = function (price, cash) {
+  let returnValue = cash - price;
+
+  // Values are defined in cents
+  const currency = [
+    { currencyType: "hundred", currencyValue: 100, count: 0 },
+    { currencyType: "fifty", currencyValue: 50, count: 0 },
+    { currencyType: "twenty", currencyValue: 20, count: 0 },
+    { currencyType: "ten", currencyValue: 10, count: 0 },
+    { currencyType: "five", currencyValue: 5, count: 0 },
+    { currencyType: "dollar", currencyValue: 1, count: 0 },
+    { currencyType: "quarter", currencyValue: 0.25, count: 0 },
+    { currencyType: "dime", currencyValue: 0.1, count: 0 },
+    { currencyType: "nickel", currencyValue: 0.05, count: 0 },
+    { currencyType: "penny", currencyValue: 0.01, count: 0 },
+  ];
+
+  let amount;
+
+  for (let i = 0; i < currency.length; i++) {
+    amount = Math.floor(returnValue / currency[i]["currencyValue"]);
+    if (amount > 0) {
+      currency[i]["count"] = amount;
+      returnValue = returnValue % currency[i]["currencyValue"];
+    }
+  }
+
+  return currency;
+};
+
+function handleClickEvent() {
+  chaChing.play();
+  let price = document.getElementById("amount-due").value;
+  let cash = document.getElementById("amount-received").value;
+  let result = calculateChange(price, cash);
+  //   document.getElementById("result").innerHTML = `${result}`;
+  console.log(result);
+  for (let i = 0; i < result.length; i++) {
+    document.getElementById(result[i]["currencyType"]).innerHTML =
+      result[i]["count"];
+  }
+  document.getElementById("currency").innerHTML = `$${(cash - price).toFixed(
+    2
+  )}`;
+}
+
+let calcButton = document.getElementById("calculate-change");
+
+calcButton.addEventListener("click", handleClickEvent);
+
+console.log(calculateChange(1787, 2000));
+console.log(calculateChange(2623, 4000));
+console.log(calculateChange(501, 1000));
---
+++ test/main.spec.js
@@ -0,0 +1,153 @@
+const express = require("express");
+const path = require("path");
+const Nightmare = require("nightmare");
+const expect = require("chai").expect;
+const axios = require("axios");
+
+const app = express();
+app.use(express.static(path.join(__dirname, "../")));
+app.listen(8888);
+
+const url = "http://localhost:8888/index.html";
+
+describe("Change Calculator", function () {
+  this.timeout(5000);
+  this.slow(3000);
+
+  it("should load successfully", () =>
+    axios.get(url).then((r) => expect(r.status === 200)));
+
+  describe("HTML", () => {
+    let pageObject;
+
+    before(() => {
+      pageObject = Nightmare().goto(url);
+    });
+
+    it('should have a H1 with the text "Change Calculator"', () =>
+      pageObject
+        .evaluate(() => document.querySelector("h1").innerText)
+        .then((heading) => expect(heading).to.equal("Change Calculator")));
+
+    it('should have an input element with an id of "amount-due"', () =>
+      pageObject
+        .evaluate(() => document.querySelector("#amount-due"))
+        .then((input) => expect(input).to.exist));
+
+    it('should have an input element with an id of "amount-received"', () =>
+      pageObject
+        .evaluate(() => document.querySelector("#amount-received"))
+        .then((input) => expect(input).to.exist));
+
+    it('should contain a button with an id of "calculate-change"', () =>
+      pageObject
+        .evaluate(() => document.querySelector("#calculate-change"))
+        .then((input) => expect(input).to.exist));
+
+    [
+      "hundred",
+      "fifty",
+      "twenty",
+      "ten",
+      "five",
+      "dollar",
+      "quarter",
+      "dime",
+      "nickel",
+      "penny",
+    ].map((id) => {
+      it(`should contain a paragraph element with an id of "${id}"`, () =>
+        pageObject
+          .evaluate((id) => document.querySelector(`#${id}`), id)
+          .then((input) => expect(input).to.exist));
+    });
+  });
+
+  describe("Integration", () => {
+    let pageObject;
+
+    beforeEach(() => {
+      pageObject = Nightmare();
+    });
+
+    it(`should display correct change`, () => {
+      return pageObject
+        .goto(url)
+        .type("#amount-received", "20")
+        .type("#amount-due", "10.21")
+        .click("#calculate-change")
+        .wait("#hundred")
+        .evaluate(() => ({
+          hundred: document.querySelector("#hundred").innerText,
+          fifty: document.querySelector("#fifty").innerText,
+          twenty: document.querySelector("#twenty").innerText,
+          ten: document.querySelector("#ten").innerText,
+          five: document.querySelector("#five").innerText,
+          dollar: document.querySelector("#dollar").innerText,
+          quarters: document.querySelector("#quarter").innerText,
+          dimes: document.querySelector("#dime").innerText,
+          nickels: document.querySelector("#nickel").innerText,
+          penny: document.querySelector("#penny").innerText,
+        }))
+        .end()
+        .then((change) => {
+          expect(change.hundred).to.equal(
+            "9",
+            "Expected hundreds didn't match"
+          );
+          expect(change.fifty).to.equal("3", "Expected fiftys didn't match");
+          expect(change.twenty).to.equal("0", "Expected twentys didn't match");
+          expect(change.ten).to.equal("0", "Expected tens didn't match");
+          expect(change.five).to.equal("4", "Expected fives didn't match");
+          expect(change.dollar).to.equal("9", "Expected dollars didn't match");
+          expect(change.quarter).to.equal(
+            "3",
+            "Expected quarters didn't match"
+          );
+          expect(change.dime).to.equal("0", "Expected dimes didn't match");
+          expect(change.nickel).to.equal("0", "Expected nickels didn't match");
+          expect(change.penny).to.equal("4", "Expected pennies didn't match");
+        });
+    });
+
+    it(`should display correct change`, () => {
+      return pageObject
+        .goto(url)
+        .type("#amount-received", "20")
+        .type("#amount-due", "13.34")
+        .click("#calculate-change")
+        .wait("#hundred")
+        .evaluate(() => ({
+          hundred: document.querySelector("#hundred").innerText,
+          fifty: document.querySelector("#fifty").innerText,
+          twenty: document.querySelector("#twenty").innerText,
+          ten: document.querySelector("#ten").innerText,
+          five: document.querySelector("#five").innerText,
+          dollar: document.querySelector("#dollar").innerText,
+          quarters: document.querySelector("#quarter").innerText,
+          dimes: document.querySelector("#dime").innerText,
+          nickels: document.querySelector("#nickel").innerText,
+          penny: document.querySelector("#penny").innerText,
+        }))
+        .end()
+        .then((change) => {
+          expect(change.hundred).to.equal(
+            "9",
+            "Expected hundreds didn't match"
+          );
+          expect(change.fifty).to.equal("3", "Expected fiftys didn't match");
+          expect(change.twenty).to.equal("0", "Expected twentys didn't match");
+          expect(change.ten).to.equal("0", "Expected tens didn't match");
+          expect(change.five).to.equal("4", "Expected fives didn't match");
+          expect(change.dollar).to.equal("9", "Expected dollars didn't match");
+          expect(change.quarter).to.equal(
+            "3",
+            "Expected quarters didn't match"
+          );
+          expect(change.dime).to.equal("0", "Expected dimes didn't match");
+          expect(change.nickel).to.equal("0", "Expected nickels didn't match");
+          expect(change.penny).to.equal("4", "Expected pennies didn't match");
+        });
+    });
+  });
+});
